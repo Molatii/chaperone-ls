445699d6c45878f861f0eb0569a6bd2ed03e8983
Molati <86184198+Molati@users.noreply.github.com>
2023-02-11 16:17:55 -0800

    commit add chakra

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <Box className="App">      
+      <h3>ls</h3>
+    </Box>
+  );
+}
+
+export default App;
